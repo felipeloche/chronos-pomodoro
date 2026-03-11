be52352521c9f134da893ae0c8421c92c8800ac9
felipeloche <felipe.loche@icloud.com>
2026-03-11 18:07:32 -0300

    Aula 3 - Uso de CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Heading } from './components/Heading'
+
 import './styles/theme.css'
 import './styles/global.css'
 
 export function App() {
 
     return (
-    <div>
-        <h1>Pomodoro Timer</h1>
+    <>
+        <Heading />
         <p>Welcome to the Pomodoro Timer app!</p>
-    </div>
+    </>
     );
 }
