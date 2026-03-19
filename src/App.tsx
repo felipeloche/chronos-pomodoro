7140205c54872d08a9c49e53e49b2b7b51b8e5b4
felipeloche <felipe.loche@icloud.com>
2026-03-19 10:00:41 -0300

    Aula 18 - Componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Heading } from './components/Heading'
 
 import './styles/theme.css'
 import './styles/global.css'
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
 
     return (
     <>
-        <Heading attr={123} attr2='string'>
-            Pomodoro Timer
+        <Heading>
+            Pomodoro Timer 
+            <button>
+                <TimerIcon/>
+            </button>
         </Heading>
         <p>Welcome to the Pomodoro Timer app!</p>
     </>
